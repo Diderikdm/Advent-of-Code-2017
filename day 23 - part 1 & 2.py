61e6593f8f5a5473702095a3b5b6e84d1ec408c6
Diderikdm <59435489+Diderikdm@users.noreply.github.com>
2021-11-05 16:51:10 +0100

    Update day 23 - part 1 & 2.py

--- day 23 - part 1 & 2.py
+++ day 23 - part 1 & 2.py
@@ -3,14 +3,14 @@ with open("day23.txt", 'r') as file:
     for registers in [{'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0}, {'a': 1, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0}]:
         muls = 0
         i = 0
-        h = 0
         while i in range(len(data)):
             if registers['a'] == 1 and data[i][0] == 'jnz':
                 sorted_regs = sorted(registers.items(), key=lambda x: x[1])
                 if sorted_regs[-3][1] % 2 == 1 and sorted_regs[-3][1] * 2 - sorted_regs[-2][1] == sorted_regs[0][-1]:
+                    registers['h'] = 0
                     for x in range(sorted_regs[-2][1], sorted_regs[-1][1] + 1, 17):
                         if any(x % y == 0 for y in range(2, x)):
-                            h += 1
+                            registers['h'] += 1
                     break
             if data[i][0] == 'set':
                 registers[data[i][1]] = registers[data[i][2]] if not data[i][2].strip('-').isdigit() else int(data[i][2])
@@ -26,4 +26,4 @@ with open("day23.txt", 'r') as file:
         if registers['a'] == 0:
             print(muls)
         else:
-            print(h)
+            print(registers['h'])
